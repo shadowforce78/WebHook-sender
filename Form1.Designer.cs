d1c01cc67dc132eeae749fa664e3f66701d8adbf
shadowforce78 <planque.adam@gmail.com>
2023-03-13 01:48:51 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WebhookSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(614, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send Webhook";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(779, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Webhook Sender By DLX | SaumonDeLuxe#2960";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(361, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Message";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 236);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(361, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "WebHook";
+            // 
+            // Form1
+            // 
+            this.AccessibleDescription = "Little program made in 1 hour";
+            this.AccessibleName = "WebHook sender";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "WebHook sender";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebhookSender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        static void SendMs(string message, string webhook)
+
+        {
+
+            WebClient client = new WebClient();
+            client.Headers.Add("Content-Type", "application/json");
+            // payload for embed
+            string payload = "{\"content\": \"" + message + "\"}";
+            client.UploadData(webhook, Encoding.UTF8.GetBytes(payload));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // if textbox1 is empty, mesasge box error
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Cant send an empty message");
+                return;
+            }
+
+            // if textbox2 dont have the right form error
+            if (textBox2.Text == " ")
+            {
+                MessageBox.Show("Cant send an empty message");
+                return;
+            }
+            if (textBox2.Text == "WebHook")
+            {
+                MessageBox.Show("Invalide WebHook");
+                return;
+            }
+
+
+
+
+            // send message to webhook
+            SendMs(this.textBox1.Text, this.textBox2.Text);
+
+
+        }
+    }
+}
+
+
+// https://discord.com/api/webhooks/890659594177765456/pyJ0flQw9jBr2umV06ZNwO4x6aOCsHcHqLC9Q5ANyn5NYkkqbUEE2l5SnUaeJRGPVN2D
