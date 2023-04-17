d5f0e054c1230e5bfe6841e8afc40ea0b9656017
shadowforce78 <planque.adam@gmail.com>
2023-04-17 14:59:22 +0200

    CheckBox making...

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Drawing;
 using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
@@ -21,20 +23,20 @@ namespace WebhookSender
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
         }
-        static void SendMs(string title, string webhook, string author, string description)
+        static void SendMs(string webhookLink)
 
         {
 
             WebClient client = new WebClient();
             client.Headers.Add("Content-Type", "application/json");
             // payload for embed
-            string payload = "{\"embeds\":[{\"title\":" + title + ",\"author\":{\"name\":" + author + "},\"description\":" + description + ",}]}";
-            client.UploadData(webhook, Encoding.UTF8.GetBytes(payload));
+            string payload = null;
+            client.UploadData(webhookLink, Encoding.UTF8.GetBytes(payload));
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SendMs(titleBox.Text, textBox2.Text, authorBox.Text, descriptionBox.Text);
+         //   SendMs();
 
         }
 
@@ -62,5 +64,30 @@ namespace WebhookSender
         {
 
         }
+
+        private Point FourthLocation;
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (checkBox1.Checked)
+            {
+                authorBox.Visible = false;
+                descriptionBox.Location = authorBox.Location;
+                if (FourthLocation == null)
+                {
+                    FourthLocation = descriptionBox.Location;
+                }
+                
+            }
+            else
+            {
+                authorBox.Visible = true;
+                descriptionBox.Location = FourthLocation;
+                
+            }
+        }
     }
 }
+
+//https://discord.com/api/webhooks/1097130504068800573/8gzatFV4vFn5C3txEPPPGyohaE1GznBFwS4X9GxWG3AUCyTCwoR-qLwM5QCsHTn9mZh4
