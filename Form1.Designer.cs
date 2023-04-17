bddd74d346d122e1aaffe7b89e7ee57b02d358dc
shadowforce78 <planque.adam@gmail.com>
2023-04-17 19:09:05 +0200

    Sender payload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,17 +35,15 @@
             this.authorBox = new System.Windows.Forms.TextBox();
             this.descriptionBox = new System.Windows.Forms.TextBox();
             this.colorBox = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // sendBtn
             // 
             this.sendBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
             this.sendBtn.ForeColor = System.Drawing.Color.White;
-            this.sendBtn.Location = new System.Drawing.Point(921, 563);
-            this.sendBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sendBtn.Location = new System.Drawing.Point(614, 366);
             this.sendBtn.Name = "sendBtn";
-            this.sendBtn.Size = new System.Drawing.Size(261, 111);
+            this.sendBtn.Size = new System.Drawing.Size(174, 72);
             this.sendBtn.TabIndex = 0;
             this.sendBtn.Text = "Send Webhook";
             this.sendBtn.UseVisualStyleBackColor = false;
@@ -56,80 +54,69 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(18, 14);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1187, 61);
+            this.label1.Size = new System.Drawing.Size(779, 39);
             this.label1.TabIndex = 1;
             this.label1.Text = "Webhook Sender By DLX | SaumonDeLuxe#2960";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(84, 605);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Location = new System.Drawing.Point(56, 393);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(540, 26);
+            this.textBox2.Size = new System.Drawing.Size(361, 20);
             this.textBox2.TabIndex = 3;
             this.textBox2.Text = "WebHook Link";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // titleBox
             // 
-            this.titleBox.Location = new System.Drawing.Point(84, 108);
+            this.titleBox.Location = new System.Drawing.Point(56, 70);
+            this.titleBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.titleBox.Name = "titleBox";
-            this.titleBox.Size = new System.Drawing.Size(540, 26);
+            this.titleBox.Size = new System.Drawing.Size(361, 20);
             this.titleBox.TabIndex = 4;
             this.titleBox.Text = "Title";
             this.titleBox.TextChanged += new System.EventHandler(this.titleBox_TextChanged);
             // 
             // authorBox
             // 
-            this.authorBox.Location = new System.Drawing.Point(84, 220);
+            this.authorBox.Location = new System.Drawing.Point(56, 143);
+            this.authorBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.authorBox.Name = "authorBox";
-            this.authorBox.Size = new System.Drawing.Size(540, 26);
+            this.authorBox.Size = new System.Drawing.Size(361, 20);
             this.authorBox.TabIndex = 5;
             this.authorBox.Text = "Author";
             this.authorBox.TextChanged += new System.EventHandler(this.authorBox_TextChanged);
             // 
             // descriptionBox
             // 
-            this.descriptionBox.Location = new System.Drawing.Point(84, 282);
+            this.descriptionBox.Location = new System.Drawing.Point(56, 183);
+            this.descriptionBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.descriptionBox.Name = "descriptionBox";
-            this.descriptionBox.Size = new System.Drawing.Size(540, 26);
+            this.descriptionBox.Size = new System.Drawing.Size(361, 20);
             this.descriptionBox.TabIndex = 6;
             this.descriptionBox.Text = "Descritption";
             this.descriptionBox.TextChanged += new System.EventHandler(this.descriptionBox_TextChanged);
             // 
             // colorBox
             // 
-            this.colorBox.Location = new System.Drawing.Point(84, 162);
+            this.colorBox.Location = new System.Drawing.Point(56, 105);
+            this.colorBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.colorBox.Name = "colorBox";
-            this.colorBox.Size = new System.Drawing.Size(540, 26);
+            this.colorBox.Size = new System.Drawing.Size(361, 20);
             this.colorBox.TabIndex = 7;
             this.colorBox.Text = "Color";
             this.colorBox.TextChanged += new System.EventHandler(this.colorBox_TextChanged);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.BackColor = System.Drawing.Color.White;
-            this.checkBox1.Location = new System.Drawing.Point(887, 163);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(88, 24);
-            this.checkBox1.TabIndex = 8;
-            this.checkBox1.Text = "Disable";
-            this.checkBox1.UseVisualStyleBackColor = false;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // Form1
             // 
             this.AccessibleDescription = "Little program made in 1 hour";
             this.AccessibleName = "WebHook sender";
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
-            this.ClientSize = new System.Drawing.Size(1200, 692);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.colorBox);
             this.Controls.Add(this.descriptionBox);
             this.Controls.Add(this.authorBox);
@@ -137,7 +124,6 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.sendBtn);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "WebHook sender";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -155,7 +141,6 @@
         private System.Windows.Forms.TextBox authorBox;
         private System.Windows.Forms.TextBox descriptionBox;
         private System.Windows.Forms.TextBox colorBox;
-        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WebhookSender
 {
@@ -19,24 +18,20 @@ namespace WebhookSender
         {
 
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-        }
-        static void SendMs(string webhookLink)
+        static void SendMs(string webhookLink, string titleBox, string authorBox, string descriptionBox)
 
         {
 
             WebClient client = new WebClient();
             client.Headers.Add("Content-Type", "application/json");
             // payload for embed 
-            string payload = null;
+            string payload = "{\"embeds\":[{\"title\":"+titleBox+",\"description\":"+descriptionBox+",\"author\":{\"name\":"+authorBox+"}}]}";
             client.UploadData(webhookLink, Encoding.UTF8.GetBytes(payload));
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-         //   SendMs();
+            SendMs(textBox2.Text, titleBox.Text, authorBox.Text, descriptionBox.Text);
 
         }
 
@@ -65,29 +60,6 @@ namespace WebhookSender
 
         }
 
-        private Point FourthLocation;
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            
-            if (checkBox1.Checked)
-            {
-                authorBox.Visible = false;
-                descriptionBox.Location = authorBox.Location;
-                if (FourthLocation == null)
-                {
-                    FourthLocation = descriptionBox.Location;
-                }
-                
-            }
-            else
-            {
-                authorBox.Visible = true;
-                descriptionBox.Location = FourthLocation;
-                
-            }
-        }
     }
 }
-
-//https://discord.com/api/webhooks/1097130504068800573/8gzatFV4vFn5C3txEPPPGyohaE1GznBFwS4X9GxWG3AUCyTCwoR-qLwM5QCsHTn9mZh4
